c71b1deaebce187b2e5f974e9dc4ebda4af35f5f
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-16 06:10:59 +0100

    fetch data from kaggle, process data

--- traffic.py
+++ traffic.py
@@ -11,4 +11,27 @@ from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 os.chdir('/Users/diamondnicholas/Desktop/Train')
 
-print(os.getcwd())
+
+data = []
+labels = []
+# We have 43 Classes
+classes = 43
+cur_path = os.getcwd()
+
+print(cur_path)
+
+# processing images
+for i in range(classes):
+    path = os.path.join(cur_path,'.',str(i))
+    images = os.listdir(path)
+    for a in images:
+        try:
+            image = Image.open(path + '\\'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
+        except Exception as e:
+            print(e)
+
+print(len(data))
